382e98c1967ce37f5024e11048af75b3a0491e3e
epaule <mh6@sanger.ac.uk>
2022-04-21 14:11:46 +0100

    initial commit

---
+++ gx_map_wrapper.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+# USAGE:gx_mapper_wrapper.bash -f fasta1 -f fasta2 -o outdir -t taxid ... or something
+# * use something like that: bsub -q long -o lsf.log -M 683000 -n 48 -R'select[mem>683000, tmp>500G] rusage[mem=683000, tmp=600G]' gx_wrapper.bash -f /my/fasta.fa.gz -o /my/outdir/ -t 1234
+# * as /tmp tends to be either SSD or tmpfs, there is a likelyhood, that we can get away with specifying less memory in the bsub 
+# * uses realpath , which should be installed at Sanger by default
+
+usage() { 
+cat << EOF
+Usage: $0 [REQUIRED -f input.fasta, -t taxonid -o outputdir]
+
+REQUIRED:
+	-f [file] Fasta file (can be specified more than once)
+	-t [int]  NCBI taxonomy id
+	-o [dir]  output directory
+OPTIONAL:
+	-h show this help
+EOF
+	exit 1
+}
+
+while getopts "f:t:ho:" opt; do
+    case $opt in
+        f ) multi+=("$OPTARG");;
+        t ) TAXID=$OPTARG;;
+	o ) OUTDIR=`realpath $OPTARG`;;
+	h | *) usage;;
+    esac
+done
+
+SIF=/lustre/scratch123/tol/teams/grit/mh6/singularity/cgr-fcs-genome.sif
+DISK=/lustre/scratch124/tol/projects/asg/sub_projects/ncbi_decon
+SINGULARITY=/lustre/scratch123/tol/teams/grit/mh6/singularity/cgr-fcs-genome.sif
+
+# speed up things
+GXDB="/tmp/gx_mapper/$$"
+mkdir -p $GXDB
+cp $DISK/* $GXDB/
+
+# so it can run in parallel
+mkdir -p $GXDB/bin
+cp -r /lustre/scratch123/tol/teams/grit/mh6/ncbi-decon/bleh/* $GXDB/bin
+
+for f in "${multi[@]}"; do
+	fasta = `realpath $f`
+	# check if file does exist
+	if [[ ! -f $fasta ]]; then
+		echo "$fasta doesn't exist"
+		rm -rf $GXDB
+		exit 1
+	fi
+	
+	FASTADIR=${fasta%/*}
+	FASTANAME=${fasta##*/}
+
+	singularity exec -B $GXDB/bin:/app/bin,$GXDB:/app/db/gxdb,$FASTADIR:/sample-volume,$OUTDIR:/output-volume $SINGULARITY python3 /app/bin/run_gx --fasta /sample-volume/$FASTANAME --out-dir /output-volume --gx-db /app/db/gxdb/all --tax-id $TAXID --debug --split-fasta
+done
+
+rm -rf $GXDB
---
+++ terrance.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+# USAGE: terrance.bash fasta outdir
+# * uses /tmp to store the db
+# * runs on the test database
+# * uses pwd as additional db storage
+# * replace the test-only with all and experience a terabyte download
+# * taxid is the one of the target species, all other hits will be reported
+# * the thing writes into /app/bin which is read-only in Singulrity, so i pulled the binaries out and mount the directory r/w (which is bad)
+
+prepend_path MODULEPATH /software/modules/
+module load ISG/singularity/
+
+SINGULARITY_TMPDIR=/lustre/scratch123/tol/teams/grit/mh6/singularity/
+SINGULARITY_CACHEDIR=$SINGULARITY_TMPDIR
+
+DB=all
+FASTADIR=${1%/*}
+FASTANAME=${1##*/}
+
+echo "FASTADIR $FASTADIR"
+echo "FASTANAME $FASTANAME"
+
+OUTDIR=$2
+SIF=/lustre/scratch123/tol/teams/grit/mh6/singularity/cgr-fcs-genome.sif
+GXDB=/lustre/scratch124/tol/projects/asg/sub_projects/ncbi_decon
+DISK=/lustre/scratch124/tol/projects/asg/sub_projects/ncbi_decon
+SINGULARITY=/lustre/scratch123/tol/teams/grit/mh6/singularity/cgr-fcs-genome.sif
+TAXID=$3
+
+# could be submitted with [select tmp>500000] rusage[tmp=500000]
+
+# docker run --name retrieve_db -v $GXDB:/app/db/gxdb cgr-fcs-genome python3 /app/bin/retrieve_db --gx-db /app/db/gxdb/test-only
+echo singularity exec -B $GXDB:/app/db/gxdb,$DISK:/db-disk-volume $SINGULARITY python3 /app/bin/retrieve_db --gx-db /app/db/gxdb/$DB --gx-db-disk /db-disk-volume/
+
+# docker run --name run_gx -v $GXDB:/app/db/gxdb -v $FASTADIR:/sample-volume -v $OUTDIR:/output-volume ncbi/cgr-fcs-genome:v1alpha1-latest python3 /app/bin/run_gx --fasta /sample-volume/fcsgenome_test.fa.gz --out-dir /output-volume --gx-db /app/db/gxdb/test-only --tax-id 6973 --debug --split-fasta
+singularity exec -B /lustre/scratch123/tol/teams/grit/mh6/ncbi-decon/bleh:/app/bin,$GXDB:/app/db/gxdb,$FASTADIR:/sample-volume,$OUTDIR:/output-volume $SINGULARITY python3 /app/bin/run_gx --fasta /sample-volume/$FASTANAME --out-dir /output-volume --gx-db /app/db/gxdb/$DB --tax-id $TAXID --debug --split-fasta
