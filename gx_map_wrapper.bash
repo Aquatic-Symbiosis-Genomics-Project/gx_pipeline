f285f666e1b9ee072ed41e7e4190d17bd4d7a6fb
Michael Paulini <mh6@sanger.ac.uk>
2022-04-22 13:44:03 +0100

    Update gx_map_wrapper.bash

--- gx_map_wrapper.bash
+++ gx_map_wrapper.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE:gx_mapper_wrapper.bash -f fasta1 -f fasta2 -o outdir -t taxid ... or something
+# USAGE:gx_mapper_wrapper.bash -f fasta1.gz -f fasta2.gz -o outdir -t taxid ... or something
 # * use something like that: bsub -q normal -o lsf.log -M 128000 -n 48 -R'select[mem>128000, tmp>500G] rusage[mem=128000, tmp=600G]' gx_wrapper.bash -f /my/fasta.fa.gz -o /my/outdir/ -t 1234
 # * as /tmp tends to be either SSD or tmpfs, there is a likelyhood, that we can get away with specifying less memory in the bsub 
 # * uses realpath , which should be installed at Sanger by default
@@ -9,7 +9,7 @@ cat << EOF
 Usage: $0 [REQUIRED -f input.fasta, -t taxonid -o outputdir]
 
 REQUIRED:
-	-f [file] Fasta file (can be specified more than once)
+	-f [file] gzipped Fasta file (can be specified more than once)
 	-t [int]  NCBI taxonomy id
 	-o [dir]  output directory
 OPTIONAL:
