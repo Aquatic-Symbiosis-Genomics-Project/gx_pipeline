dd6e5dc25f0d639b2777d14c6a204ba5bd4ae34c
Michael Paulini <mh6@sanger.ac.uk>
2022-06-07 16:11:09 +0100

    Update gx_map_wrapper.bash

--- gx_map_wrapper.bash
+++ gx_map_wrapper.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # USAGE:gx_mapper_wrapper.bash -f fasta1.gz -f fasta2.gz -o outdir -t taxid ... or something
-# * use something like that: bsub -q normal -o lsf.log -M 128000 -n 48 -R'select[mem>128000, tmp>500G] rusage[mem=128000, tmp=600G]' gx_wrapper.bash -f /my/fasta.fa.gz -o /my/outdir/ -t 1234
+# * use like that: bsub -q normal -o lsf.log -M 128000 -n 48 -R'select[mem>128000, tmp>500G] rusage[mem=128000, tmp=600G]' gx_wrapper.bash -f /my/fasta.fa.gz -o /my/outdir/ -t 1234
 # * as /tmp tends to be either SSD or tmpfs, there is a likelyhood, that we can get away with specifying less memory in the bsub 
 # * uses realpath , which should be installed at Sanger by default
-# * $DISK is where the database is stored
+# * make sure to enable the singularity env
 
 usage() { 
 cat << EOF
@@ -30,9 +30,7 @@ done
 
 DISK=/lustre/scratch124/tol/projects/asg/sub_projects/ncbi_decon
 SINGULARITY=/lustre/scratch123/tol/teams/grit/mh6/singularity/gx-develop-latest.sif
-GXSCRIPTS=/lustre/scratch123/tol/teams/grit/mh6/ncbi-decon/singularity_test/gx_pipeline
 
-# speed up things .. that would be also SHM_LOC
 GXDB="/tmp/gx_mapper/$$/gxdb"
 mkdir -p $GXDB
 
@@ -40,7 +38,7 @@ for file in "${multi[@]}"; do
 	fasta=`realpath $file`
 	# check if file does exist
 	if [[ -f $fasta ]]; then
-		python3 $GXSCRIPTS/run_fcsgenome.py --fasta $fasta --out-dir $OUTDIR --gx-db "${GXDB}/all" --gx-db-disk $DISK --split-fasta --tax-id $TAXID --container-engine=singularity --image=$SINGULARITY
+		python3 run_fcsgenome.py --fasta $fasta --out-dir $OUTDIR --gx-db "${GXDB}/all" --gx-db-disk $DISK --split-fasta --tax-id $TAXID --container-engine=singularity --image=$SINGULARITY
 	else
 		echo "$fasta doesn't exist"
 	fi
